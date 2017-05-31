5f69c52e8d4ec59563f4aa6e535d1158c85f9011
Todd Briley <todd.briley@outlook.com>
2017-05-30 21:40:29 -0400

    added mapping with autolocation and makers for recent purchases

--- test/src/components/Map.js
+++ test/src/components/Map.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import { View } from 'react-native';
 import MapView from 'react-native-maps';
+import axios from 'axios';
 
 class Map extends React.Component {
     constructor(props) {
@@ -14,27 +15,54 @@ class Map extends React.Component {
                 longitude: -84.3730464,
                 latitudeDelta: 0.05,
                 longitudeDelta: 0.05,
-            }
+            },
+            purchases: []
         }
     }
 
-    // componentDidMount() {
-    //     navigator.geolocation.getCurrentPosition(
-    //         (position) => {
-    //             var initialPosition = JSON.stringify(position);
-    //             this.setState({ initialLocation: initialPosition });
-    //             console.log('Initial Position', initialPosition);
-    //         }
-    //     )
-    // }
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                var initialPosition = JSON.stringify(position);
+                var newLocation = {latitude: position.coords.latitude,
+                                  longitude: position.coords.longitude,
+                                  latitudeDelta: 0.1,
+                                  longitudeDelta: 0.1
+                                }
+                this.setState({ initialLocation: newLocation });
+                // console.log('Initial Position', initialPosition);
+            }
+        );
+
+        axios.post('http://et.briley.org/api/expenseswithlocation', {
+          token: this.props.token
+        })
+        .then(expenses => {
+          this.setState({ purchases: expenses.data });
+        })
+        .catch(err => console.log(err.message));
+
+    }
+
+    createMarkers(){
+      let markers = this.state.purchases.map(purchase => <MapView.Marker coordinate={{latitude: purchase.latitude, longitude: purchase.longitude }} title={purchase.description} description={purchase.amount} image={require('./Resources/maps-and-flags.png')} />);
+      return markers;
+
+    }
 
     render() {
+      console.log('expenses: ', this.state.purchases);
         return (
             <View style={styles.container}>
                 <MapView
                     style={styles.map}
-                    initialRegion={this.state.initialLocation}>
-                  <MapView.Marker
+                    initialRegion={this.state.initialLocation}
+                    region={this.state.initialLocation}
+                    showsMyLocationButton="true"
+                    rotateEnabled="false"
+                    >
+                    {this.createMarkers()}
+                  {/* <MapView.Marker
                     coordinate={{
                         latitude: 33.8499041,
                         longitude: -84.3730464,
@@ -44,6 +72,16 @@ class Map extends React.Component {
                     image={require('./Resources/maps-and-flags.png')}
 
                   />
+                  <MapView.Marker
+                    coordinate={{
+                        latitude: 33.84993,
+                        longitude: -84.3734,
+                    }}
+                    title={'Chipotle'}
+                    description={'$5.70'}
+                    image={require('./Resources/maps-and-flags.png')}
+
+                  /> */}
                 </MapView>
             </View>
         )
